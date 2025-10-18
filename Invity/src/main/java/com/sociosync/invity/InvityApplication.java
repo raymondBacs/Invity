cbc80126e9e159dc75d12226eee33da3a4e487d2
raymondBacs <140140116+raymondBacs@users.noreply.github.com>
2025-10-19 07:30:24 +0800

    Initial Commit

---
+++ Invity/src/main/java/com/sociosync/invity/InvityApplication.java
@@ -0,0 +1,13 @@
+package com.sociosync.invity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvityApplication.class, args);
+	}
+
+}
---
+++ Invity/src/test/java/com/sociosync/invity/InvityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sociosync.invity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
